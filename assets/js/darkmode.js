f3388b1e6853b6cf1075992c9fa1cdaa44c197cf
Eliseo Cutrona <eliseocutrona@gmail.com>
2022-10-04 23:38:22 -0300

    estilos nuevarama

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,18 +1,21 @@
-import { preferenciaModo, botonMode, bodyTag, setDark, setLight, persistirDark, persistirLight  } from "./modules/var_darkmode.js";
+import {
+  preferenciaModo,
+  botonMode,
+  bodyTag,
+  setDark,
+  setLight,
+  persistirDark,
+  persistirLight,
+} from "./modules/var_darkmode.js";
 
 //revisa si hay un modo preferido guardado
 
-
 window.onload = () => {
-    preferenciaModo === "dark" ? setDark() : setLight();
-}
-
+  preferenciaModo === "dark" ? setDark() : setLight();
+};
 
 //cambia entre modos y setea la preferencia
- botonMode.onclick = () => {
-    let modo = bodyTag.getAttribute('class');
-    modo === "light" ? persistirDark() : persistirLight();
- }
-
-
-
+botonMode.onclick = () => {
+  let modo = bodyTag.getAttribute("class");
+  modo === "light" ? persistirDark() : persistirLight();
+};
