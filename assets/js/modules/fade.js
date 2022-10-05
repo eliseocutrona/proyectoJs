30294e7a36ec23863be640173109127af6c73e9f
Eliseo Cutrona <eliseocutrona@gmail.com>
2022-10-05 00:24:14 -0300

    estilos nuevarama

--- assets/js/modules/fade.js
+++ assets/js/modules/fade.js
@@ -1,21 +1,23 @@
-export function fadeInSlow(ele){
-    ele.style.opacity = 0;
-    function aparecer() {
-        ele.style.opacity =+ ele.style.opacity + 0.01; //velocidad del fade
+export function fadeInSlow(ele) {
+  ele.style.opacity = 0;
+  function aparecer() {
+    ele.style.opacity = +ele.style.opacity + 0.01; //velocidad del fade
     if (ele.style.opacity < 1) {
-        (window.requestAnimationFrame && requestAnimationFrame(aparecer)) || setTimeout(aparecer, 16)
-        };
+      (window.requestAnimationFrame && requestAnimationFrame(aparecer)) ||
+        setTimeout(aparecer, 16);
     }
-    aparecer();
+  }
+  aparecer();
 }
 
-export function fadeInFast(ele){
-    ele.style.opacity = 0;
-    function aparecer() {
-        ele.style.opacity =+ ele.style.opacity + 0.04; //velocidad del fade
+export function fadeInFast(ele) {
+  ele.style.opacity = 0;
+  function aparecer() {
+    ele.style.opacity = +ele.style.opacity + 0.04; //velocidad del fade
     if (ele.style.opacity < 1) {
-        (window.requestAnimationFrame && requestAnimationFrame(aparecer)) || setTimeout(aparecer, 16)
-        };
+      (window.requestAnimationFrame && requestAnimationFrame(aparecer)) ||
+        setTimeout(aparecer, 16);
     }
-    aparecer();
+  }
+  aparecer();
 }
